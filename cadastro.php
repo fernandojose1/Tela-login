694bd5e9bdde2ba4e9561b2a87a9108cd1b7cc81
fernandojose1 <69250217+fernandojose1@users.noreply.github.com>
2021-03-09 22:23:18 -0300

    Initial commit

---
+++ cadastro.php
@@ -0,0 +1,38 @@
+<?php 
+$login = $_POST['login'];
+$senha = md5($_POST['senha']);
+$connect = mysqli_connect('localhost:3306','root','','crud01');
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysqli_query($connect,$query_select);
+$total = mysqli_num_rows($select);
+
+  if($login == "" || $login == null){
+      echo"<script language='javascript' type='text/javascript'>
+          alert('O login precisa ser preenchido! ');window.location.
+          href='cadastro.html'</script>";
+
+    }else{
+      if($total>0){
+
+        echo"<script language='javascript' type='text/javascript'>
+          alert('Esse login já existe! ');window.location.
+          href='cadastro.html'</script>";
+        die();
+
+      }else{
+        $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+        $insert = mysqli_query($connect,$query);
+
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Usuário cadastrado com sucesso!');window.location.
+          href='login.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Não foi possível cadastrar esse usuário, tente novamente!');window.location
+          .href='cadastro.html'</script>";
+        }
+      }
+    }
+
+ ?>
